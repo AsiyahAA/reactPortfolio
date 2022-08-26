a1c869862011b1191855f99110a5190ef64118f1
asiya <asiyaahmed33@yahoo.com>
2022-08-25 18:08:26 -0700

    cleaning up code

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function Resume() {
   return (
-    // <header className="header">
       <a href='https://asiyahaa.github.io/my-resume/'>View my resume Here: </a>
-    // </header>
   );
 }
 
